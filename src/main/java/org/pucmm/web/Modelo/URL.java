cf1959d79beebeca43181c4bb6510650b2a65387
jean <jean18699@gmail.com>
2021-07-07 01:13:42 -0400

    prueba 3

--- src/main/java/org/pucmm/web/Modelo/URL.java
+++ src/main/java/org/pucmm/web/Modelo/URL.java
@@ -1,7 +1,10 @@
 package org.pucmm.web.Modelo;
 
+import org.pucmm.web.Servicio.GestionDb;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -13,40 +16,16 @@ public class URL implements Serializable {
     private String direccionAcortada; //direccion acortada
     private String origen; //Origen que se va a acortar
 
-  /*  @Column(name = "cantidad_visitas")
-    private long visitas = 0L;
-
-    //Variables para indicar la cantidad de veces que ha sido accedido dependiendo del navegador
-    @Column(name = "Chrome")
-    private Long chrome = 0L;
-
-    @Column(name = "Firefox")
-    private Long firefox = 0L;
-
-    @Column(name = "Safari")
-    private Long safari = 0L;
-
-    @Column(name = "Opera")
-    private Long opera = 0L;
-
-    @Column(name = "Edge")
-    private Long edge = 0L;
-
-    @Column(name = "Internet_Explorer")
-    private Long internetExplorer = 0L;
-
-    //Para pruebas, postman
-    @Column(name = "Postman")
-    private Long postman = 0L;
-
-    @Column(name = "Navegador_desconocido")
-    private Long unknownBrowser = 0L;
-*/
     @OneToMany(fetch = FetchType.EAGER)
     private Set<Cliente> clientes;
 
 
-    public URL() {}
+    public URL() {
+        if(clientes == null)
+        {
+            clientes = new HashSet<>();
+        }
+    }
 
     public String getOrigen() {
         return origen;
@@ -63,81 +42,15 @@ public class URL implements Serializable {
     public void setDireccionAcortada(String direccion) {
         this.direccionAcortada = direccion;
     }
-/*
-    public long getVisitas() {
-        return visitas;
-    }
 
-    public void setVisitas(long visitas) {
-        this.visitas = visitas;
-    }
-
-    public Long getChrome() {
-        return chrome;
-    }
-
-    public void setChrome(Long chrome) {
-        this.chrome = chrome;
-    }
-
-    public Long getFirefox() {
-        return firefox;
-    }
-
-    public void setFirefox(Long firefox) {
-        this.firefox = firefox;
-    }
-
-    public Long getSafari() {
-        return safari;
-    }
-
-    public void setSafari(Long safari) {
-        this.safari = safari;
-    }
-
-    public Long getOpera() {
-        return opera;
-    }
-
-    public void setOpera(Long opera) {
-        this.opera = opera;
-    }
-
-    public Long getEdge() {
-        return edge;
-    }
-
-    public void setEdge(Long edge) {
-        this.edge = edge;
-    }
-
-    public Long getInternetExplorer() {
-        return internetExplorer;
-    }
-
-    public void setInternetExplorer(Long internetExplorer) {
-        this.internetExplorer = internetExplorer;
-    }
-
-    public Long getPostman() {
-        return postman;
-    }
-
-    public void setPostman(Long postman) {
-        this.postman = postman;
-    }
-
-    public Long getUnknownBrowser() {
-        return unknownBrowser;
-    }
-
-    public void setUnknownBrowser(Long unknown) {
-        this.unknownBrowser = unknown;
-    }
-*/
     public Set<Cliente> getClientes() {
         return clientes;
+    }
+
+    public long getCantidadClientes()
+    {
+        GestionDb gestionURL = new GestionDb(URL.class);
+        return ((URL) gestionURL.find(this.direccionAcortada)).getClientes().size();
     }
 
     public void setClientes(Set<Cliente> clientes) {
